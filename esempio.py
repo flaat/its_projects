5472ee1cdf71842101da614c142ac2cb1d3a42e3
flaat <giorgi.flavio.at@gmail.com>
2024-06-19 13:41:23 +0200

    minor

--- esempio.py
+++ esempio.py
@@ -29,7 +29,6 @@ def merge(list_1: list[int], list_2: list[int]) -> list[int]:
             
             if j == len(list_2):
                 
-                
                 return result[:k+1] + list_1[i:]
             
         else:
@@ -43,15 +42,52 @@ def merge(list_1: list[int], list_2: list[int]) -> list[int]:
                 return result[:k+1] + list_2[j:]
 
 
+def bubble_sort_v2(x: list[int]):
+    # Ω(n) -> caso migliore quando la lista è già ordinata
+    # O(n^2) -> caso peggiore
+    ho_fatto_swap: bool = True
+    for i in range(len(x)):
+        for j in range(len(x) - i - 1):
+            if x[j] > x[j+1]:
+                # swap(x[j], x[j+1])
+                ho_fatto_swap = False
+                temp: int = x[j]
+                x[j] = x[j+1]
+                x[j+1] = temp
+        if ho_fatto_swap:
+            break
+
 if __name__ == "__main__":
     
     import random
+    import time
+    import matplotlib.pyplot as plt
     
-    list_input: list[int] = [random.randint(0, 100000) for _ in range(10000)]
-    
-    result: list[int] = mergeSort(list_input=list_input)
-
-    print(result)
+    merge_sort_times: list[float] = []
+    bubble_sort_times: list[float] = []
+    cases = [10, 50, 100, 250, 500, 1000, 1500, 2000, 5000, 10000, 20000]
+    for n in cases:
+        
+        list_input: list[int] = [random.randint(0, 100000) for _ in range(n)]
+        
+        start = time.time()    
+        result: list[int] = mergeSort(list_input=list_input)
+        end = time.time()
+        
+        merge_sort_times.append(end-start)
+            
+        start = time.time()
+        result: list[int] = bubble_sort_v2(x=list_input)
+        end = time.time()
+        
+        bubble_sort_times.append(end-start)
+        
+    print(f"MERGE: {merge_sort_times}\nBUBBLE: {bubble_sort_times}")
+    plt.plot(cases, merge_sort_times, label="Merge")
+    plt.plot(cases, bubble_sort_times, label="Bubble")
+    plt.xscale("log")
+    plt.legend()
+    plt.show()
 # class Esempio:
     
 #     def __init__(self, name) -> None:
